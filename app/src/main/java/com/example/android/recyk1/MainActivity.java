d9857fa75f7438aa131938b19186379bb85aab8d
kevinmugiira <kevinm5740@aol.com>
2020-02-11 22:00:46 +0300

    initial commit

---
+++ app/src/main/java/com/example/android/recyk1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.recyk1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+
+    String s1[], s2[];
+    int images[] = {R.drawable.clogo,R.drawable.cpluslogo, R.drawable.javalogo, R.drawable.jslogo, R.drawable.kotlinlogo, R.drawable.pythonlogo, R.drawable.rubylogo, R.drawable.swiftlogo, R.drawable.typescriptlogo, R.drawable.phplogo};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        s1 = getResources().getStringArray(R.array.programming_languages);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
